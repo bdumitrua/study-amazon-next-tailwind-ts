1815d63bd57e8bcb693c128586a03ab645b3ae05
Buzulan Dumitru <bdumitrua@gmail.com>
2023-03-18 21:47:07 +0300

    front start

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="en">  
+    <Head>
+      <link rel="icon" href="/favicon.svg" sizes='any' type="image/svg+xml" />
+    </Head>
       <body>
         <Main />
         <NextScript />
